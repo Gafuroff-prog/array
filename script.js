0340867cde009920136a471f5491c9729fa2817c
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-11-22 18:33:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(`iteration number: ${i}`);
+// }
+
+// let count = 0;
+// while (count < 3) {
+//     console.log(`counter: ${count}`);
+//     count++;
+// }
+
+let count = 5;
+do {
+    console.log(`counter: ${count}`);
+    count++;
+}while (count < 3);
